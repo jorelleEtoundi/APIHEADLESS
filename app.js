d402d8e2c64848ccbbfd497571f6169939e48b52
jorelleEtoundi <jorelle.etoundi@gmail.com>
2024-12-02 16:00:04 +0100

    [DEL] [ADD]

--- app.js
+++ app.js
@@ -19,8 +19,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.post("/signup", async (req, res) => {
+  const { username, password, role } = req.body;
+  const allowedRoles = ["user", "content-creator"];
+  if (!allowedRoles.includes(role)) {
+    return res.status(400).send("Invalid role");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const request = new sql.Request();
+
+  try {
+    await request.query(
+      `INSERT INTO Users (username, password, role) VALUES ('${username}', '${hashedPassword}', '${role}')`
+    );
+    res.send("Signup successful");
+    console.log(`User ${username} signed up with role ${role}`);
+  } catch (err) {
+    res.status(500).send("Error signing up");
+    console.error("Error signing up:", err);
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
